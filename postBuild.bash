5d9691a9de0388780f76e2ed31e262c170d5a411
Brian Furtaw <brian.furtaw@gmail.com>
2025-01-23 10:59:37 -0500

    Modified 2 files
    - Modified files: code/FineTuning-SDXL.ipynb, postBuild.bash

--- postBuild.bash
+++ postBuild.bash
@@ -11,7 +11,7 @@ git clone https://github.com/huggingface/diffusers && \
     pip install -e .
 cd -
 
-pip install peft==0.9.0 huggingface_hub[cli,torch]==0.21.4
+pip install peft==0.9.0 huggingface_hub[cli,torch]==0.27.1
 
 sudo mkdir -p /mnt/cache/
 sudo chown workbench:workbench /mnt/cache/
